c5c98370c83397b910c7e4e30fe704bbc6c1ffe7
Bui Van Thai <buithaild230899@gmail.com>
2022-07-15 16:57:25 +0700

    CRUD-READ

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,22 +1,22 @@
 const e = require('express');
 const Course = require('../models/Course');
-
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    index(req, res) {
-
-        Course.find({}, function (err, courses) {
-            if (!err) {
-                res.json(courses);
-            } else {
-                res.status(400).json({ err: 'ERROR!!!' });
-            }
-
-        });
+    index(req, res, next) {
+        Course.find({})
+            .then(courses => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses)
+                });
+            })
+            .catch(next)
 
         // res.render('home');
     }
+
+
 
     // [GET] /search
     search(req, res) {
