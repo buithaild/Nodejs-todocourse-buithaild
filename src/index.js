f6765f07ab3bb76794c9428881bec2785e1d7d09
Bui Van Thai <buithaild230899@gmail.com>
2022-07-15 11:55:29 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 7000;
+
+const route = require('./routers');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+//http logger
+// app.use(morgan('combined'))
+
+//template engine
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources', 'views'));
+
+//Routes init
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
